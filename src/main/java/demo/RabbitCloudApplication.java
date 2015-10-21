7d9d52855d7c0c5891267316489eefd487b5faca
Utkarsh Nadkarni <unadkarni@UtkarshkarniMBP.home>
2015-10-20 22:16:44 -0400

    Removed conflicts during merge

--- src/main/java/demo/RabbitCloudApplication.java
+++ src/main/java/demo/RabbitCloudApplication.java
@@ -4,20 +4,16 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudFactory;
-import org.springframework.cloud.service.common.AmqpServiceInfo;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 @ComponentScan
@@ -35,45 +31,6 @@ public class RabbitCloudApplication {
 	private RabbitTemplate rabbitTemplate;
 
 	
-<<<<<<< HEAD
-=======
-	@Configuration
-	@Profile("dev")
-	public static class DevContext {
-		// dev beans
-		
-		@Bean
-		public ConnectionFactory connectionFactory() {
-			// Read the host, port, username and password from a config file
-			CachingConnectionFactory connectionFactory = new CachingConnectionFactory(
-					"localhost");
-			connectionFactory.setUsername("guest");
-			connectionFactory.setPassword("guest");
-			return connectionFactory;
-		}
-	}
-
-	@Configuration
-	@Profile("cloud")
-	public static class CloudContext {
-		// cloud beans
-
-		@Profile("cloud")
-		@Bean
-		public ConnectionFactory connectionFactory() {
-			
-			CloudFactory cloudFactory = new CloudFactory();
-			// Return a cloud suitable for the current environment
-			Cloud cloud = cloudFactory.getCloud();
-			AmqpServiceInfo serviceInfo = (AmqpServiceInfo) cloud
-					.getServiceInfo("my-rabbit");
-			String serviceID = serviceInfo.getId();
-			return cloud.getServiceConnector(serviceID,
-					ConnectionFactory.class, null);
-		}
-	}
-	
->>>>>>> 8681f6587361e02a3855e089afd8d027825d48d9
 	// default beans
 	@Bean
 	public RabbitTemplate rabbitTemplate() {
