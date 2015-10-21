8e6ab1dbc520e36e40f78a9157862d5d553f3038
Utkarsh Nadkarni <unadkarni@UtkarshkarniMBP.home>
2015-10-20 22:23:54 -0400

    Fixed compilation errors

--- src/main/java/demo/RabbitCloudController.java
+++ src/main/java/demo/RabbitCloudController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
@@ -13,12 +14,13 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 public class RabbitCloudController {
 	
 	static Logger logger = Logger.getLogger(RabbitCloudController.class);
+	
+	@Autowired
+	RabbitTemplate rabbitTemplate;
     
 	@RequestMapping(value="/publish", method=RequestMethod.GET)
 	public @ResponseBody String publish(String message){
 		logger.info("In publish");
-		ApplicationContext ctx = RabbitCloudApplication.getApplicationContext();
-		RabbitTemplate rabbitTemplate = ctx.getBean(RabbitTemplate.class);
 		rabbitTemplate.convertAndSend(message);
 		logger.info("Publishing..." + message);
 		return "Published " + message;
@@ -27,8 +29,6 @@ public class RabbitCloudController {
 	@RequestMapping(value="/receive", method=RequestMethod.GET)
 	public @ResponseBody String receive(){
 		logger.info("In receive");
-		ApplicationContext ctx = RabbitCloudApplication.getApplicationContext();
-		RabbitTemplate rabbitTemplate = (RabbitTemplate) ctx.getBean(RabbitTemplate.class);
 		String message = (String)rabbitTemplate.receiveAndConvert(RabbitCloudApplication.queueName);
 		if(message == null) {
 			message = "No messages in queue " + RabbitCloudApplication.queueName;
